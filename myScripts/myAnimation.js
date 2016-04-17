ce5f11a6af68d8230fe7facd5487d340a7750315
tijojose27 <tijojose27@yahoo.com>
2016-04-17 11:06:04 -0500

    added everthing from previos project and updated it

---
+++ myScripts/myAnimation.js
@@ -0,0 +1,18 @@
+﻿$(function () {
+    // MAIN SITE FADING 
+    $("body").removeClass("fade-out");
+
+    //TRANSITION IN MAIN SECTION  --not implementing cuz cant get it to work--
+    /*
+    setTimeout(function () {
+        $('#cover').chocolate({
+            images: ['../Images/Background/cuttree.jpg', '../Images/Background/kitchen.jpg', '../Images/Background/tree_alter.jpg'
+                , '../Images/Background/walkway.jpg', '../Images/Background/bath.jpg'],
+            interval: 4000,
+            speed: 2000
+        });
+    },5000);
+    */
+
+
+});
